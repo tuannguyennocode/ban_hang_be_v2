3855e882134c53ef97b979cf628eeda9b86560f7
Tuan Nguyen <tuanbmt123123@gmail.com>
2024-01-18 10:24:52 +0700

    add env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,20 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 
+const ENV = process.env.NODE_ENV;
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      isGlobal: true,
+    }),
+    MongooseModule.forRoot(process.env.DB_HOST, {
+      dbName: process.env.DB_NAME,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
