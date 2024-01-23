e3bc6764c9effc02834adec2bb5e697ab9bd523c
Tuan Nguyen <tuanbmt123123@gmail.com>
2024-01-23 15:18:00 +0700

    fix deploy

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ async function bootstrap() {
     app.enableCors({
         origin: true,
         methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-        credentials: true, // Cho phép truy cập có danh tính (đối với các cookie hoặc chứng thực)
+        credentials: true,
     });
     await app.listen(5000);
 }
