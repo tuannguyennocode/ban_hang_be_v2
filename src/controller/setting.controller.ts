2903517f014f4d0d5e1c41e40d325300f70606ab
Tuan Nguyen <tuanbmt123123@gmail.com>
2024-03-01 16:00:56 +0700

    public setting

--- src/controller/setting.controller.ts
+++ src/controller/setting.controller.ts
@@ -2,6 +2,7 @@ import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { CreateSettingDto } from '../dto/setting/create-setting.dto';
 import { UpdateSettingDto } from '../dto/setting/update-setting.dto';
 import { SettingService } from '../service/setting.service';
+import { Public } from '../constant';
 
 @Controller('setting')
 export class SettingController {
@@ -12,6 +13,7 @@ export class SettingController {
         return this.settingService.create(createSettingDto);
     }
 
+    @Public()
     @Get()
     findAll() {
         return this.settingService.findAll();
